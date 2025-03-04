b4d07c4a6bfc5d5c04da65de8248fc24310e58b9
artaKEK <artem.artamon0ff@yandex.ru>
2025-03-04 13:11:10 +0300

    added bind twitter metod

---  main/main.py
+++  main/main.py
@@ -1,7 +1,10 @@
+import base64
+
 import aiohttp
 
 import asyncio
 
+import twitter
 
 from fake_useragent import FakeUserAgent
 
@@ -27,24 +30,36 @@ headers = {
 
 
 class Account:
-    def __init__(self, mail: str, password: str, cookies: dict | None = None, headers: dict | None = None, private_key: str | None = None):
+    def __init__(self, mail: str | None = None, password: str | None = None, cookies: dict | None = None, headers: dict | None = None, private_key: str | None = None):
         self.mail = mail
         self.password = password
         self.cookies = cookies
         self.headers = headers
         self.wallet = Wallet(private_key=private_key)
 
-    async def post_request_with_headers_and_cookies(self, url: str, json_data: dict):
+    async def request(self, url: str, type_request: str,json_data: dict | None = None, proxy: str | None = None, params: dict | None = None, for_json: bool = False):
 
         async with aiohttp.ClientSession() as session:
 
-            async with session.post(url, json=json_data, headers=self.headers) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    return data
-                else:
-                    raise Exception(f"Ошибка при выполнении запроса: {response.status}, {await response.text()}")
+            if type_request == 'post':
+                async with session.post(url, json=json_data, headers=self.headers, proxy=proxy) as response:
+                    if response.status == 200:
+                        data = await response.json()
+                        return data
+                    else:
+                        raise Exception(f"Ошибка при выполнении запроса: {response.status}, {await response.text()}")
 
+            elif type_request == 'get':
+                async with session.get(url, headers=self.headers, proxy=proxy, params=params) as response:
+                    if response.status == 200:
+                        if for_json:
+                            return response
+                        data = await response.json()
+                        return data
+                    else:
+                        raise Exception(f"Ошибка при выполнении запроса: {response.status}, {await response.text()}")
+            else:
+                print('Check type request')
 
     async def register(self):
         url = 'https://castile.world/api/guest/register'  # Укажите ваш URL
@@ -55,11 +70,10 @@ class Account:
             'password_confirm': self.password,
             'agree': True,
         }
-        response_data = await self.post_request_with_headers_and_cookies(url, json_data)
+        response_data = await self.request(url, 'post', json_data)
         authorization = f'Bearer {response_data["data"]["auth"]}'
         self.headers['authorization'] = authorization
         print(response_data)
-
 
     async def login(self):
         url = 'https://castile.world/api/guest/login'
@@ -67,7 +81,7 @@ class Account:
             'email': self.mail,
             'password': self.password,
         }
-        response_data = await self.post_request_with_headers_and_cookies(url, json_data)
+        response_data = await self.request(url, 'post', json_data)
         authorization =f'Bearer {response_data["data"]["auth"]}'
         self.headers['authorization'] = authorization
         print(response_data)
@@ -75,7 +89,7 @@ class Account:
     async def quantity_points(self):
         url = 'https://castile.world/api/task/pointsInfo'
         json_data = {}
-        response_data = await self.post_request_with_headers_and_cookies(url, json_data)
+        response_data = await self.request(url, 'post', json_data)
         print(response_data['data']['points'])
 
     async def bind_wallet(self):
@@ -88,13 +102,59 @@ class Account:
             'message': self.wallet.generate_massage(),
             'type': 'Aptos',
         }
-        response_data = await self.post_request_with_headers_and_cookies(url, json_data)
+        response_data = await self.request(url=url, type_request= 'post', json_data=json_data)
         print(response_data)
 
+    async def get_user_info(self) -> dict:
+        url = 'https://castile.world/api/user/pullUserInfo'
+        response_data = await self.request(url, 'post')
+        return response_data
+
+    async def bind_twitter(self, auth_token: str | None,  proxy: str | None = None):
+
+        url = 'https://www.castile.world/api/twitter/receive'
+
+        data = await self.get_user_info()
+        uid = data.get('data', {}).get('uid')
+
+        decoded_string = f'{uid}|bind_twitter|'
+
+        byte_string = decoded_string.encode('utf-8')
+
+        encoded_bytes = base64.b64encode(byte_string)
+
+        state = encoded_bytes.decode('utf-8')
+
+        auth_code_params = {
+            'response_type': 'code',
+            'client_id': 'VktCa2VJcjVsdkZoSnFDLUpHanQ6MTpjaQ',
+            'redirect_uri': 'https://www.castile.world/api/twitter/receive',
+            'scope': 'users.read tweet.read',
+            'state': state,
+            'code_challenge': 'challenge',
+            'code_challenge_method': 'plain',
+        }
+
+        twitter_account = twitter.Account(auth_token=auth_token)
+
+        async with twitter.Client(twitter_account, proxy=proxy) as twitter_client:
+            auth_code = await twitter_client.oauth2(**auth_code_params)
+
+        params = {
+            'state': state,
+            'code': auth_code,
+        }
+
+        response_data = await self.request(url=url, type_request='get',params=params, for_json=True)
+        print(response_data)
+
+
 async def main():
-    account = Account('zxcqweasd123456789@yandex.ru', 'A12fsaiki43fj', headers=headers)
+    account = Account('zxcqweasd202526@yandex.ru', 'A12fsaiki43fj', headers=headers)
     await account.login()
     # await account.bind_wallet()
+    # await account.bind_twitter(auth_token='6173c4866966a15249685e21d800bdd281ddb15f', proxy='212.116.242.66:7486:user210578:q67d4i')
+    print(await account.get_user_info())
 
 if __name__ == '__main__':
     asyncio.run(main())
---  main/test.py
+++  main/test.py
@@ -1,10 +1,25 @@
+import asyncio
 import base64
+from main import Account
 
-from aptos_sdk.account import Account
-from aptos_sdk.ed25519 import Signature
+account = Account()
 
-private_key = '0x16bd6732fbd8344dafc7398bd075ca17edb1c1eb29db5787574d528eabfa06b5'
+async def fun():
 
-account = Account.load_key(private_key)
+    decoded_string = f'{uid}|bind_twitter|'
 
-print(type(str(account.public_key())))
+
+    byte_string = decoded_string.encode('utf-8')
+
+    # Кодируем байты в формат Base64
+    encoded_bytes = base64.b64encode(byte_string)
+
+    # Преобразуем обратно в строку для удобства
+    encoded_string = encoded_bytes.decode('utf-8')
+
+    # Вывод результата
+    print(decoded_string)
+    print(encoded_string)
+
+if __name__ == '__main__':
+    asyncio.run(fun())
